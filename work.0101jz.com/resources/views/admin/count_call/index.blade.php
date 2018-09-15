c50e27387e4ef65dd084fe030b5f8a86edc7d2b0
刘欣 <380007897@qq.com>
2018-09-15 11:30:08 +0800

    图表的简单封装

--- work.0101jz.com/resources/views/admin/count_call/index.blade.php
+++ work.0101jz.com/resources/views/admin/count_call/index.blade.php
@@ -11,14 +11,10 @@
 		<div class="mmhead" id="mywork">
 			<div class="tabbox" >
 				<a href="#">按天统计</a> <a href="" >每月统计</a>
-
 			</div>
 		</div>
-		<div class="tubiao">
-			<img src="{{asset('staticadmin/images/tb1.png')}}" />
-
-			<p>2018-05-01--- 2018-05-28(今天)</p>
-		</div>
+		<div class="tubiao" id="container" style="height: 100%;height: 500px"></div>
+		<p>2018-05-01--- 2018-05-28(今天)</p>
 		<table class="table2">
 			<thead>
 			<tr>
@@ -55,9 +51,6 @@
 				<td>2015-04-22</td>
 				<td>233</td>
 			</tr>
-
-
-
 			</tbody>
 		</table>
 
@@ -69,4 +62,20 @@
 @endpush
 
 @push('footlast')
+{{--这个js是引用echarts的插件的js--}}
+<script type="text/javascript" src="http://echarts.baidu.com/gallery/vendors/echarts/echarts.min.js"></script>
+<script src="{{asset('js/adminpublic/graph.js')}}"></script>
+<script type="text/javascript">
+	var dataAxis = ['点', '击', '柱', '子', '或', '者', '两', '指', '在', '触', '屏', '上', '滑', '动', '能', '够', '自', '动', '缩', '放'];
+	var data = [220, 182, 191, 234, 290, 330, 310, 123, 442, 321, 90, 149, 210, 122, 133, 334, 198, 123, 125, 220];
+	var yMax = 500;
+	var id = "container";
+	/*
+	* ymax: 柱状图阴影部分的高度(如果数值高于左边的数字，左边的数字也会增长)
+	* dataAxis：柱状图上的显示的文字(一维数组)
+	* data：柱状图上的数值(一维数组)
+	* id：div的id名
+	* */
+	zhuzhuangtu(yMax,data,dataAxis,id);
+</script>
 @endpush
--- work.0101jz.com/resources/views/admin/count_customer/index.blade.php
+++ work.0101jz.com/resources/views/admin/count_customer/index.blade.php
@@ -10,17 +10,17 @@
 	<div class="mm">
 		<div class="tubiao"  >
 
-			<p>客户组成表(企业、个人、...)</p>
+			{{--<p>客户组成表(企业、个人、...)</p>--}}
 
-			<img src="{{asset('staticadmin/images/tb2.png')}}" />
+			<div id="container1" style="height: 600px;width: 100%"></div>
 
-			<p>客户区域分布表(区域)</p>
-			<img src="{{asset('staticadmin/images/tb1.png')}}" />
+			{{--<p>客户区域分布表(区域)</p>--}}
+			<div id="container" style="height: 600px;width: 100%"></div>
 
 
-			<p>工单类型表</p>
+			{{--<p>工单类型表</p>--}}
 
-			<img src="{{asset('staticadmin/images/tb3.png')}}" />
+			<div id="container3" style="height: 600px;width: 100%"></div>
 
 		</div>
 
@@ -32,4 +32,60 @@
 @endpush
 
 @push('footlast')
+{{--这个js是引用echarts的插件的js--}}
+<script type="text/javascript" src="http://echarts.baidu.com/gallery/vendors/echarts/echarts.min.js"></script>
+<script src="{{asset('js/adminpublic/graph.js')}}"></script>
+<script type="text/javascript">
+
+	var id = "container1";
+	var data = [
+		{value:335, name:'直接访问'},
+		{value:310, name:'邮件营销'},
+		{value:234, name:'联盟广告'},
+		{value:135, name:'视频广告'},
+		{value:1548, name:'搜索引擎'}
+	];
+	var title = '客户组成表';
+	var leftTitle = ['直接访问','邮件营销','联盟广告','视频广告','搜索引擎'];
+	/*
+	* id:div中id的值
+	* title：图表中正上方的名称
+	* data：饼状图中的名称和数据
+	* leftTitle: 右上角标识的显示的名称
+	* */
+	bingzhuangtu(id,title,data,leftTitle);
+	var id = "container3";
+	var data = [
+		{value:335, name:'直接访问'},
+		{value:310, name:'邮件营销'},
+		{value:234, name:'联盟广告'},
+		{value:135, name:'视频广告'},
+		{value:1548, name:'搜索引擎'}
+	];
+	var title = '工单类型表';
+	var leftTitle = ['直接访问','邮件营销','联盟广告','视频广告','搜索引擎'];
+
+	/*
+	 * id:div中id的值
+	 * title：图表中正上方的名称
+	 * data：饼状图中的名称和数据
+	 * leftTitle: 右上角标识的显示的名称
+	 * */
+	bingzhuangtu(id,title,data,leftTitle);
+</script>
+
+<script type="text/javascript">
+	var dataAxis = ['点', '击', '柱', '子', '或', '者', '两', '指', '在', '触', '屏', '上', '滑', '动', '能', '够', '自', '动', '缩', '放'];
+	var data = [220, 182, 191, 234, 290, 330, 310, 123, 442, 321, 90, 149, 210, 122, 133, 334, 198, 123, 125, 220];
+	var yMax = 500;
+	var id = "container";
+	var title = "客户区域分布表(区域)";
+	/*
+	 * ymax: 柱状图阴影部分的高度(如果数值高于左边的数字，左边的数字也会增长)
+	 * dataAxis：柱状图上的显示的文字(一维数组)
+	 * data：柱状图上的数值(一维数组)
+	 * id：div的id名
+	 * */
+	zhuzhuangtu(yMax,data,dataAxis,id,title);
+</script>
 @endpush
