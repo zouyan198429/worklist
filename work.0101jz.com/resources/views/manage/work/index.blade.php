5d4bd2cce4844927185a6016d6789f195f510c7b
workservice <305463219@qq.com>
2018-09-28 18:22:10 +0800

    吕东兴修改

--- work.0101jz.com/resources/views/manage/work/index.blade.php
+++ work.0101jz.com/resources/views/manage/work/index.blade.php
@@ -43,15 +43,19 @@
 			<thead>
 			<tr>
 				<th>工单号</th>
-				<th>下单时间</th>
-				<th>派单人员</th>
-				<th>维修人员</th>
-				<th>工单等级</th>
-				<th>状态</th>
 				<th>来电号码</th>
-				<th>客户姓名</th>
-				<th>客户类别</th>
+				<th>工单来源</th>
+				<th>工单类型</th>
+				<th>工单内容</th>
 				<th>客户位置</th>
+				<th>下单时间</th>
+				<th>工单等级</th>
+				<th>派单人员</th>
+				<th>区县客服</th>
+				<th>状态</th>
+				<!-- 
+				<th>客户姓名</th>
+				<th>客户类别</th> -->
 				<th>操作</th>
 			</tr>
 			</thead>
@@ -69,72 +73,7 @@
 				<td>企业</td>
 				<td>秦州区/中城街道</td>
 				<td><a href="" class="btn btn-gray" >查看</a></td>
-			</tr>
-			<tr>
-				<td>345236864</td>
-				<td>05-22  15:33</td>
-				<td>张芸</td>
-				<td>雷栋栋</td>
-				<td>2小时</td>
-				<td>剩余1小时12分</td>
-				<td><a href="tel:15366658554" class="btn" >15366658554 <i class="fa fa-phone-square fa-fw" aria-hidden="true"></i> </a></td>
-				<td>王(女)</td>
-				<td>企业</td>
-				<td>秦州区/中城街道</td>
-				<td><a href="" class="btn btn-gray" >查看</a></td>
-			</tr>
-			<tr>
-				<td>345236864</td>
-				<td>05-22  15:33</td>
-				<td>张芸</td>
-				<td>雷栋栋</td>
-				<td>2小时</td>
-				<td><span class="red" >超时</span></td>
-				<td><a href="tel:15366658554" class="btn" >15366658554 <i class="fa fa-phone-square fa-fw" aria-hidden="true"></i> </a></td>
-				<td>王(女)</td>
-				<td>企业</td>
-				<td>秦州区/中城街道</td>
-				<td><a href="" class="btn btn-gray" >查看</a></td>
-			</tr>
-			<tr>
-				<td>345236864</td>
-				<td>05-22  15:33</td>
-				<td>张芸</td>
-				<td>雷栋栋</td>
-				<td>2小时</td>
-				<td>剩余1小时12分</td>
-				<td><a href="tel:15366658554" class="btn" >15366658554 <i class="fa fa-phone-square fa-fw" aria-hidden="true"></i> </a></td>
-				<td>王(女)</td>
-				<td>企业</td>
-				<td>秦州区/中城街道</td>
-				<td><a href="" class="btn btn-gray" >查看</a></td>
-			</tr>
-			<tr>
-				<td>345236864</td>
-				<td>05-22  15:33</td>
-				<td>张芸</td>
-				<td>雷栋栋</td>
-				<td>2小时</td>
-				<td>剩余1小时12分</td>
-				<td><a href="tel:15366658554" class="btn" >15366658554 <i class="fa fa-phone-square fa-fw" aria-hidden="true"></i> </a></td>
-				<td>王(女)</td>
-				<td>企业</td>
-				<td>秦州区/中城街道</td>
-				<td><a href="" class="btn btn-gray" >查看</a></td>
-			</tr>
-			<tr>
-				<td>345236864</td>
-				<td>05-22  15:33</td>
-				<td>张芸</td>
-				<td>雷栋栋</td>
-				<td>2小时</td>
-				<td>剩余1小时12分</td>
-				<td><a href="tel:15366658554" class="btn" >15366658554 <i class="fa fa-phone-square fa-fw" aria-hidden="true"></i> </a></td>
-				<td>王(女)</td>
-				<td>企业</td>
-				<td>秦州区/中城街道</td>
-				<td><a href="" class="btn btn-gray" >查看</a></td>
-			</tr>
+			</tr> 
 			--}}
 			</tbody>
 		</table>
